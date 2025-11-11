6f87351b733781eab6f36a07639be758983cdfab
Gunjan Jalan <deepesh.ahirwar100@gmail.com>
2025-11-11 14:56:29 +0530

    this is my first commit on github adding vecadd.py file..

---
+++ vecadd.py
@@ -0,0 +1,11 @@
+a1=int(input("enter a1 for first vector :"))
+b1=int(input("enter b1 for first vector :"))
+a2=int(input("enter a2 for first vector :"))
+b2=int(input("enter b2 for first vector :"))
+
+print("first vector is :",a1,"i + ",b1,"j")
+print("second vector is :",a2,"i + ",b2,"j")
+
+print("product is :",a1*a2+b1*b2)
+
+print("this commit for bug fix in master branch file ")
